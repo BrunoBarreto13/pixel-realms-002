7ce63906572ec0bf9f2c9d58265952db4b2ed37f
[dyad] <cetcplan@gmail.com>
2025-11-01 13:04:49 -0300

    [dyad] Updating panel colors - wrote 1 file(s)

--- src/components/GeneralPanel.tsx
+++ src/components/GeneralPanel.tsx
@@ -9,10 +9,10 @@ interface GeneralPanelProps {
 const GeneralPanel: React.FC<GeneralPanelProps> = ({ children, className }) => {
   return (
     <div className={cn(
-      "relative mt-10 border-8 border-[#9B6B4B] bg-[#4A3C32] shadow-[inset_0_0_0_3px_#E6B88A] p-5 min-h-[calc(100vh-18rem)]",
+      "relative mt-10 border-8 border-[#8B6E46] bg-[#4A3C32] shadow-[inset_0_0_0_3px_#E6B88A] p-5 min-h-[calc(100vh-18rem)]",
       className
     )}>
-      <div className="absolute -top-[36px] left-4 bg-[#8B5A3C] text-white font-pixel text-xs uppercase px-4 py-2 border-t-4 border-x-4 border-[#9B6B4B] rounded-t-lg">
+      <div className="absolute -top-[36px] left-4 bg-[#7A5F3B] text-white font-pixel text-xs uppercase px-4 py-2 border-t-4 border-x-4 border-[#8B6E46] rounded-t-lg">
         Início
       </div>
       {children}
