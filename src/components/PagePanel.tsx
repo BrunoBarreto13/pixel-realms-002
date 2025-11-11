bf27d76e9492e2475514d880682c4cdde752b04a
[dyad] <cetcplan@gmail.com>
2025-11-10 22:06:29 -0300

    [dyad] Tornando o fundo dos painéis do dashboard transparente - wrote 2 file(s) + extra files edited outside of Dyad

--- src/components/PagePanel.tsx
+++ src/components/PagePanel.tsx
@@ -10,7 +10,7 @@ interface PagePanelProps {
 const PagePanel: React.FC<PagePanelProps> = ({ children, className, title }) => {
   return (
     <div className={cn(
-      "relative mt-10 border-8 border-[#8B6E46] bg-[#4A3C32] shadow-[inset_0_0_0_3px_#E6B88A] p-5 min-h-[calc(100vh-18rem)]",
+      "relative mt-10 border-8 border-[#8B6E46] bg-transparent shadow-[inset_0_0_0_3px_#E6B88A] p-5 min-h-[calc(100vh-18rem)]",
       className
     )}>
       <div className="absolute -top-[36px] left-4 bg-[#7A5F3B] text-white font-pixel text-xs uppercase px-4 py-2 border-t-4 border-x-4 border-[#8B6E46] rounded-t-lg">
